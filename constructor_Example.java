ec3bf60a2b59cad370cfb9a75aa537673fe27c31
Shravan Kumar Singh <38663641+Shravans821@users.noreply.github.com>
2018-04-23 23:04:27 +0530

    Add files via upload

---
+++ constructor_Example.java
@@ -0,0 +1,15 @@
+class A{
+int a;
+String s;
+A(){
+a=4;
+s="Lalu";
+System.out.println("Hello");
+}
+}
+class constructor_Example{
+public static void main(String s[]){
+A a1=new A();
+A a2=new A();
+}
+}
